bfc787b27e6d9c290d3558915370f4826c8abe7d
共同資財理工研究会イシバシステムズ(共用) <Motacilla.alba.lugens@gmail.com>
2015-11-11 01:06:53 +0900

    修正
    起動時間が1時間未満の時、分表示が乱れるのを修正

--- server_uptime.sh
+++ server_uptime.sh
@@ -2,11 +2,16 @@
 
 echo サーバーの停止…
 set -f
+  echo -n 稼働時間:
   set -- `uptime | sed -e s/,//g -e 's/:/ /g'`
   if test "$5" = "days" ; then
     line=`expr $4 * 24 + $6`:$7
   else
-    line=$4:$5
+    if test "$5" = "mins" ; then
+      line=0:$4
+    else
+      line=$4:$5
+    fi
   fi
-  echo "稼働時間: "$line
+  echo " "$line
 set +f
